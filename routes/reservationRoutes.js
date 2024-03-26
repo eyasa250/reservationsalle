2b614d1259a31bd82a6ab2294ad8860688b08276
eyasa250 <79605706+eyasa250@users.noreply.github.com>
2024-03-26 22:25:10 +0100

    create reservation

--- routes/reservationRoutes.js
+++ routes/reservationRoutes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const reservationController = require('../controllers/reservationController');
 
+// Define the route handlers
 router.get('/', reservationController.getAllReservations);
 router.post('/', reservationController.createReservation);
-// Ajoutez d'autres routes au besoin
 
 module.exports = router;
